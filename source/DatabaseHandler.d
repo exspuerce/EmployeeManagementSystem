39daec43838e18baf6e07ff14e40ba00ce9becd3
Luke Bluett <Luke.J.Bluett@gmail.com>
2018-01-16 15:11:51 +0000

    Created class to handle single employees in mongo
    modified:   source/DatabaseHandler.d
    
    DatabaseHandler class, removed read so as to create seperate class to
    handle multiple Employees, and leave EmployeeHandler only handler single
    employees.
    
    EmployeeHandler class, added setters and getters for Employee struct.
    Created override methods for create, update, delete.
    
    Application Compiles, but hasn't been tested.

--- source/DatabaseHandler.d
+++ source/DatabaseHandler.d
@@ -4,31 +4,30 @@
 */
 module DatabaseHandler;
 
+import std.stdio;
 import vibe.d;
 
 abstract class DatabaseHandler
 {
 	private:
-			MongoClient client;
+		MongoClient client;
 
 	public:
-			this ( string ip_address )
+		this ( string ip_address )
+		{
+			try
 			{
-				try
-				{
-					client = connectMongoDB ( ip_address );
-			
-				} 
-				catch ( Exception e )
-				{
-					writeln ( e );
-				}
+				client = connectMongoDB ( ip_address );
+			} 
+			catch ( Exception e )
+			{
+				writeln ( e );
 			}
+		}
 
-			abstract bool createData ();
-			abstract MongoCursor! ( Bson, Employee, typeof ( null ) ) readData ();
-			abstract bool updateData ();
-			abstract bool deleteData ();
+		abstract bool createData ();	
+		abstract bool updateData ( string identifier );
+		abstract bool deleteData ( string identifier );
 }
 
 struct Employee
@@ -37,4 +36,116 @@ struct Employee
 	string name;
 	string email;
 	string password;
+}
+
+class EmployeeHandler : DatabaseHandler
+{
+	private:
+		MongoCollection employee_collection;
+		Employee employee;
+
+	public:
+		this ( string ip_address, string collection )
+		{
+			super ( ip_address );
+			try
+			{
+				employee_collection = client.getCollection(collection);
+			}
+			catch ( Exception e )
+			{
+				writeln ( e );
+			}
+		}
+
+		override bool createData ()
+		{
+			try
+			{
+				employee_collection.insert ( employee );
+				return true;
+			}
+			catch ( Exception e )
+			{
+				writeln( e );
+				return false;
+			}
+		}
+
+		override bool updateData (string identifier)
+		{
+			try
+			{
+				employee_collection.update
+				(
+				 	[ "username" : identifier ],
+					[
+						"username" 	: getUsername(),
+						"name" 		: getName(),
+						"email" 	: getEmail(),
+						"password" 	: getPassword()
+					]
+				);
+				return true;
+			}
+			catch ( Exception e )
+			{
+				writeln ( e );
+				return false;
+			}	
+		}
+
+		override bool deleteData ( string identifier )
+		{
+			try
+			{
+				employee_collection.remove ( [ "username" : identifier ] );
+				return true;
+			} 
+			catch ( Exception e ) 
+			{
+				writeln ( e );
+				return false;
+			}
+		}
+
+		void setUsername ( string new_username )
+		{
+			employee.username = new_username;
+		}
+
+		string getUsername ()
+		{
+			return employee.username;
+		}
+
+		void setName ( string new_name )
+		{
+			employee.name = new_name;
+		}
+
+		string getName ()
+		{
+			return employee.name;
+		}
+
+		void setEmail ( string new_email )
+		{
+			employee.email = new_email;
+		}
+
+		string getEmail ()
+		{
+			return employee.email;
+		}
+
+		void setPassword ( string new_password )
+		{
+			employee.password = new_password;
+		}
+
+		string getPassword ()
+		{
+			return employee.password;
+		}
 }
